bda0c25b64f983b1184cc15c58e783ec8dbcd84d
akring <akio@mail.com>
2024-05-21 17:55:29 +0900

    modified

--- arges-appstack/lib/AppStack.ts
+++ arges-appstack/lib/AppStack.ts
@@ -1,8 +1,9 @@
-import { CustomResource, Fn, RemovalPolicy, SecretValue, Stack, StackProps } from "aws-cdk-lib";
+import { CustomResource, Duration, RemovalPolicy, ScopedAws, Stack, StackProps } from "aws-cdk-lib";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
-import { Secret } from "aws-cdk-lib/aws-secretsmanager";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
 import { Provider } from "aws-cdk-lib/custom-resources";
 import { Construct } from "constructs";
 
@@ -10,9 +11,10 @@ export class AppStack extends Stack {
 
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+        const { accountId, region, stackName, } = new ScopedAws(this);
 
         // -----
-        // prepare keypair
+        // generate and store a keypair
         // -----
         const { publicKey, privateKey } = (() => {
             const parent = new Construct(this, 'KeyPairGenerator');
@@ -20,11 +22,17 @@ export class AppStack extends Stack {
                 runtime: Runtime.NODEJS_LATEST,
                 entry: `${__dirname}/KeyPairGenerator.ts`,
                 handler: 'handler',
+                timeout: Duration.seconds(30),
                 logGroup: new LogGroup(parent, 'HandlerLog', {
                     removalPolicy: RemovalPolicy.DESTROY,
                     retention: RetentionDays.ONE_DAY,
-                })
+                }),
             });
+            handler.addToRolePolicy(new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: ['ssm:PutParameter', 'ssm:DeleteParameter'],
+                resources: [`arn:aws:ssm:${region}:${accountId}:parameter/${stackName}/*`],
+            }));
             const provider = new Provider(parent, 'Provider', {
                 onEventHandler: handler,
                 logGroup: new LogGroup(parent, 'ProviderLog', {
@@ -34,19 +42,19 @@ export class AppStack extends Stack {
             });
             const resource = new CustomResource(parent, 'CustomResource', {
                 serviceToken: provider.serviceToken,
+                removalPolicy: RemovalPolicy.DESTROY,
             });
-            return {
-                publicKey: Fn.join('\\n', Fn.split('\n', resource.getAttString('PublicKey'))),
-                privateKey: Fn.join('\\n', Fn.split('\n', resource.getAttString('PrivateKey'))),
-            };
+            const publicKey = StringParameter.fromStringParameterAttributes(parent, 'PublicKey', {
+                parameterName: resource.getAttString('PublicKey'),
+                simpleName: false,
+                forceDynamicReference: true,
+            });
+            const privateKey = StringParameter.fromSecureStringParameterAttributes(parent, 'PrivateKey', {
+                parameterName: resource.getAttString('PrivateKey'),
+                simpleName: false,
+            });
+            return { publicKey, privateKey, }
         })();
-
-        const keyPairSecret = new Secret(this, 'KeyPair', {
-            secretObjectValue: {
-                publicKey: SecretValue.unsafePlainText(publicKey),
-                privateKey: SecretValue.unsafePlainText(privateKey),
-            }
-        });
 
         // -----
         //
--- arges-appstack/lib/KeyPairGenerator.ts
+++ arges-appstack/lib/KeyPairGenerator.ts
@@ -1,9 +1,54 @@
+import { DeleteParameterCommand, ParameterType, PutParameterCommand, SSMClient } from "@aws-sdk/client-ssm";
 import { CdkCustomResourceHandler } from "aws-lambda";
 import { generateKeyPairSync } from "crypto";
 
+const client = new SSMClient();
+
+const putStringParameter = async (name: string, value: string, secure?: boolean) => {
+    const command = new PutParameterCommand({
+        Name: name,
+        Value: value,
+        Type: secure ? ParameterType.SECURE_STRING : ParameterType.STRING,
+    });
+    return await client.send(command).then(() => name)
+}
+
+const deleteStringParameter = async (name: string) => {
+    const command = new DeleteParameterCommand({ Name: name, });
+    return await client.send(command).then(() => { });
+}
+
+const putKeyPairToParameterStore = async (stackName: string, suffix: string, publicKey: string, privateKey: string) => {
+    return await Promise.allSettled([
+        putStringParameter(`/${stackName}/PublicKey-${suffix}`, publicKey),
+        putStringParameter(`/${stackName}/PrivateKey-${suffix}`, privateKey, true),
+    ]).then(results => {
+        results.forEach(item => {
+            if (item.status === 'rejected') {
+                throw new Error((item as PromiseRejectedResult).reason)
+            }
+        });
+        return Promise.resolve({
+            Data: {
+                PublicKey: (results[0] as PromiseFulfilledResult<string>).value,
+                PrivateKey: (results[1] as PromiseFulfilledResult<string>).value,
+            }
+        });
+    });
+}
+
+const deleteKeyPairFromParameterStore = async (stackName: string, suffix: string) => {
+    return await Promise.allSettled([
+        deleteStringParameter(`/${stackName}/PublicKey-${suffix}`),
+        deleteStringParameter(`/${stackName}/PrivateKey-${suffix}`),
+    ]);
+}
+
 export const handler: CdkCustomResourceHandler = async (event, context) => {
 
+    const stackName = event.StackId.split('/')[1];
     switch (event.RequestType) {
+
         case 'Create':
             const { publicKey, privateKey } = generateKeyPairSync('rsa', {
                 modulusLength: 2048,
@@ -16,15 +61,17 @@ export const handler: CdkCustomResourceHandler = async (event, context) => {
                     format: 'pem',
                 }
             });
-            return {
-                Data: {
-                    PublicKey: publicKey,
-                    PrivateKey: privateKey
-                }
-            };
+            try {
+                return await putKeyPairToParameterStore(stackName, event.RequestId, publicKey, privateKey);
+            } catch (e) {
+                await deleteKeyPairFromParameterStore(stackName, event.RequestId);
+                throw e;
+            }
+
         case 'Update':
             return {};
+
         case 'Delete':
-            return {};
+            return await deleteKeyPairFromParameterStore(stackName, event.PhysicalResourceId);
     }
 }
