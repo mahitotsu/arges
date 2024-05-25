fec66a90be93d5e25f2a2ca5e1bf3aa4019b6a4f
akring <akio@mail.com>
2024-05-25 23:46:46 +0900

    modified

--- arges-appstack/lib/EdgeLocation/signInHandler.ts
+++ arges-appstack/lib/EdgeLocation/signInHandler.ts
@@ -1,5 +1,7 @@
 import { CloudfrontSignedCookiesOutput, getSignedCookies } from "@aws-sdk/cloudfront-signer";
 import { APIGatewayProxyHandlerV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import * as jwt from 'jsonwebtoken';
+import { JwksClient } from "jwks-rsa";
 
 interface Secret {
     privateKey: string;
@@ -29,17 +31,31 @@ const secretName = process.env.SECRET_NAME!
 const secretExtensionPort = process.env.PARAMETERS_SECRETS_EXTENSION_HTTP_PORT!
 const keyPairId = process.env.KEY_PAIR_ID!
 const userPoolId = process.env.USER_POOL_ID!
+const issuer = `https://cognito-idp.${region}.amazonaws.com/${userPoolId}`;
 
 const cognitoEndpoints = (async () => {
-    return fetch(`https://cognito-idp.${region}.amazonaws.com/${userPoolId}/.well-known/openid-configuration`)
+    return fetch(`${issuer}/.well-known/openid-configuration`)
         .then(res => res.json())
         .then(json => json as Endpoints);
 })();
-const keys = (async () => {
-    return fetch((await cognitoEndpoints).jwks_uri)
-        .then(res => res.json())
-        .then(json => json);
-})();
+
+const jwksClient = (async () => { return new JwksClient({ jwksUri: (await cognitoEndpoints).jwks_uri }); })();
+const decodeToken = async (token: string, options?: jwt.VerifyOptions) => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(
+            token,
+            (header, callback) => {
+                jwksClient.then(client => {
+                    client.getSigningKey(header.kid, (err, key) => {
+                        callback(err, key?.getPublicKey());
+                    });
+                });
+            },
+            options,
+            (err, decoded) => err ? reject(err) : resolve(decoded)
+        );
+    });
+}
 
 const forbiddenResponse = {
     statusCode: 403,
@@ -82,6 +98,8 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context): Promise
     })
         .then(res => res.json())
         .then(json => json as Tokens);
+    const idToken = await decodeToken(tokens.id_token, { issuer: issuer, audience: secretValue.clientId, });
+    const accessToken = await decodeToken(tokens.access_token, { issuer: issuer, });
 
     const userInfo = await fetch(endpoints.userinfo_endpoint, {
         method: 'GET',
@@ -120,7 +138,8 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context): Promise
         cookies: cookieValues,
         body: JSON.stringify({
             event,
-            tokens,
+            idToken,
+            accessToken,
             userInfo,
         }, null, 4),
     }
