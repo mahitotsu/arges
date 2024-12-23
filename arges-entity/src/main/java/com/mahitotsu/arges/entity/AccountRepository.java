333787fe2ec4fcd26dc46ea0db6377302c9e4498
Akio Kanashiki <akio@mail.com>
2024-12-23 21:20:30 +0900

    clear

--- arges-entity/src/main/java/com/mahitotsu/arges/entity/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mahitotsu.arges.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<AccountEntity, AccountId> {
-
-}
--- arges-entity/src/test/java/com/mahitotsu/arges/entity/EntityTestBase.java
+++
@@ -1,8 +0,0 @@
-package com.mahitotsu.arges.entity;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public abstract class EntityTestBase {
-
-}
